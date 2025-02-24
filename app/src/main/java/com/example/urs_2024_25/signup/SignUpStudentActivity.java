75c8978b6186f0723dc1056be58138ebcdb5d139
igovor01 <igovor01@fesb.hr>
2025-02-24 18:09:56 +0100

    update ui

--- app/src/main/java/com/example/urs_2024_25/signup/SignUpStudentActivity.java
+++ app/src/main/java/com/example/urs_2024_25/signup/SignUpStudentActivity.java
@@ -3,7 +3,6 @@ package com.example.urs_2024_25.signup;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,7 +15,7 @@ import com.example.urs_2024_25.login.LogInStudentActivity;
 
 public class SignUpStudentActivity extends AppCompatActivity {
 
-    private EditText signupEmail, signupPassword;
+    private EditText signupName, signupSurname, signupPassword;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,32 +24,31 @@ public class SignUpStudentActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up_student);
 
-        signupEmail = findViewById(R.id.signup_student_email);
+        signupName = findViewById(R.id.signup_student_name);
+        signupSurname = findViewById(R.id.signup_student_surname);
         signupPassword = findViewById(R.id.signup_student_password);
         Button signupButton = findViewById(R.id.signup_student_button);
         TextView loginRedirectText = findViewById(R.id.login_student_redirect);
 
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String user = signupEmail.getText().toString().trim();
-                String pass = signupPassword.getText().toString().trim();
+        signupButton.setOnClickListener(v -> {
+            String name = signupName.getText().toString().trim();
+            String surname = signupSurname.getText().toString().trim();
+            String pass = signupPassword.getText().toString().trim();
 
-                if (user.isEmpty()) {
-                    signupEmail.setError("Email cannot be empty");
-                }
-                if (pass.isEmpty()) {
-                    signupPassword.setError("Password cannot be empty");
-                }
+            if (name.isEmpty()) {
+                signupName.setError("Name cannot be empty");
+            }
+            if (surname.isEmpty()) {
+                signupSurname.setError("Surname cannot be empty");
+            }
+            if (pass.isEmpty()) {
+                signupPassword.setError("Password cannot be empty");
             }
         });
 
-        loginRedirectText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SignUpStudentActivity.this, LogInStudentActivity.class));
-            }
-        });
+        loginRedirectText.setOnClickListener(v -> startActivity(
+                new Intent(SignUpStudentActivity.this,
+                        LogInStudentActivity.class)));
     }
 }
